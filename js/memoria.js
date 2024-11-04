10222d08de2fab2dd1b51835ee62e120a8aa52f9
UO277490 <uo277490@uniovi.es>
2024-11-04 12:22:30 +0100

    Actualizacion de los iconos de juego de memoria (la idea era tenerlos en local y no en la web)

--- js/memoria.js
+++ js/memoria.js
@@ -2,51 +2,51 @@ class Memoria {
     elements = [
         {
             element: 'RedBull',
-            source: 'https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg'
+            source: 'multimedia/imagenes/Red_Bull_Racing_logo.svg'
         },
         {
             element: 'McLaren',
-            source: 'https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg'
+            source: 'multimedia/imagenes/McLaren_Racing_logo.svg'
         },
         {
             element: 'Alpine',
-            source: 'https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg'
+            source: 'multimedia/imagenes/Alpine_F1_Team_2021_Logo.svg'
         },
         {
             element: 'AstonMartin',
-            source: 'https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg'
+            source: 'multimedia/imagenes/Aston_Martin_Aramco_Cognizant_F1.svg'
         },
         {
             element: 'Ferrari',
-            source: 'https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg'
+            source: 'multimedia/imagenes/Scuderia_Ferrari_Logo.svg'
         },
         {
             element: 'Mercedes',
-            source: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg'
+            source: 'multimedia/imagenes/Mercedes_AMG_Petronas_F1_Logo.svg'
         },
         {
             element: 'RedBull',
-            source: 'https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg'
+            source: 'multimedia/imagenes/Red_Bull_Racing_logo.svg'
         },
         {
             element: 'McLaren',
-            source: 'https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg'
+            source: 'multimedia/imagenes/McLaren_Racing_logo.svg'
         },
         {
             element: 'Alpine',
-            source: 'https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg'
+            source: 'multimedia/imagenes/Alpine_F1_Team_2021_Logo.svg'
         },
         {
             element: 'AstonMartin',
-            source: 'https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg'
+            source: 'multimedia/imagenes/Aston_Martin_Aramco_Cognizant_F1.svg'
         },
         {
             element: 'Ferrari',
-            source: 'https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg'
+            source: 'multimedia/imagenes/Scuderia_Ferrari_Logo.svg'
         },
         {
             element: 'Mercedes',
-            source: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg'
+            source: 'multimedia/imagenes/Mercedes_AMG_Petronas_F1_Logo.svg'
         }
     ]
 
