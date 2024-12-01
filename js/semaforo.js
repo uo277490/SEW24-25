50b5dca1f72c659102937c6dd2cb23ccc915be12
UO277490 <uo277490@uniovi.es>
2024-12-01 12:09:21 +0100

    PHP - Practica 1 - Ejercicio 2 (ranking semaforo)

--- js/semaforo.js
+++ js/semaforo.js
@@ -1,6 +1,6 @@
 class Semaforo {
-        
-    constructor (){
+
+    constructor() {
         this.levels = [0.2, 0.5, 0.8];
         this.lights = 4;
         this.unload_moment = null;
@@ -10,8 +10,8 @@ class Semaforo {
         this.createStructure();
     }
 
-    createStructure(){
-        var main  = document.querySelector('main');
+    createStructure() {
+        var main = document.querySelector('main');
         var section = document.createElement('section');
 
         var h3 = document.createElement('h3');
@@ -19,7 +19,7 @@ class Semaforo {
         section.appendChild(h3);
 
         // Se crean los div para las diferentes luces del semaforo
-        for (let i = 0; i < this.lights; i++){
+        for (let i = 0; i < this.lights; i++) {
             var div = document.createElement('div');
             section.appendChild(div);
         }
@@ -40,45 +40,84 @@ class Semaforo {
     }
 
     initSequence() {
-        var main  = document.querySelector('main');
+        var main = document.querySelector('main');
         main.setAttribute("class", "load");
         var startButton = document.querySelectorAll('button')[0];
         startButton.disabled = true;
-                
+
         setTimeout(() => {
             this.unload_moment = new Date();
             this.endSequence();
-        },2000+(this.difficulty*100));
+        }, 2000 + (this.difficulty * 100));
     }
 
-    endSequence(){
-        var main  = document.querySelector('main');
+    endSequence() {
+        var main = document.querySelector('main');
         main.setAttribute("class", "unload");
         var endButton = document.querySelectorAll('button')[1];
         endButton.disabled = false;
     }
 
-    stopReaction(){
+    stopReaction() {
         this.click_moment = new Date();
-        var time_diff = (this.click_moment - this.unload_moment)/1000;
+        var time_diff = (this.click_moment - this.unload_moment) / 1000;
 
         var sect = document.querySelector('section');
-        
+
         // Este if se utiliza para evitar un bug y que si se juega muchas veces seguidas no haya 
         // muchos parrafos en pantalla y solo el ultimo
         var existShowTime = sect.querySelector('p');
-        if(existShowTime==null){
+        if (existShowTime == null) {
             var show_time = document.createElement('p');
-            show_time.textContent = "Tiempo de reacción: "+time_diff+" s.";
+            show_time.textContent = "Tiempo de reacción: " + time_diff + " s.";
             sect.appendChild(show_time);
         } else {
-            existShowTime.textContent = "Tiempo de reacción: "+time_diff+" s.";
+            existShowTime.textContent = "Tiempo de reacción: " + time_diff + " s.";
         }
 
+        this.createRecordForm();
 
         var endButton = document.querySelectorAll('button')[1];
         endButton.disabled = true;
         var startButton = document.querySelectorAll('button')[0];
         startButton.disabled = false;
+    }
+
+    createRecordForm() {
+        let time_diff = (this.click_moment - this.unload_moment) / 1000
+        var form_sect = $('<article>').append(
+            $('<h4>').text("Guardar tiempo"),
+            $('<form>')
+                .attr("action", "semaforo.php")
+                .attr("method", "POST")
+                .append(
+                    $('<label>').text("Nombre:").append( // Nombre
+                        $('<input>').attr("type", "text")
+                            .attr("name", "nombre")
+                            .attr("placeholder", "Nombre")
+                            .attr("required", true)
+                    ),
+                    $('<label>').text("Apellidos:").append( // Apellido
+                        $('<input>').attr("type", "text")
+                            .attr("name", "apellidos")
+                            .attr("placeholder", "Apellidos")
+                            .attr("required", true)
+                    ),
+                    $('<label>').text("Tiempo:").append( // Tiempo
+                        $('<input>').attr("type", "text")
+                            .attr("name", "tiempo")
+                            .attr("value", time_diff)
+                            .attr("readonly", true)
+                    ),
+                    $('<label>').text("Dificultad:").append( // Dificultad
+                        $('<input>').attr("type", "text")
+                            .attr("name", "nivel")
+                            .attr("value", this.difficulty)
+                            .attr("readonly", true)
+                    ),
+                    $('<input>').attr("type", "submit") // Boton submit
+                        .attr("value", "Guardar")
+                ));
+        $('main').append(form_sect);
     }
 }
---
+++ semaforo.php
@@ -0,0 +1,139 @@
+<?php 
+    class Record {
+        private $server;
+        private $user;
+        private $pass;
+        private $dbname;
+        public function __construct(){
+            $this->server = "localhost";
+            $this->user = "DBUSER2024";
+            $this->pass = "DBPSWD2024";
+            $this->dbname = "records";
+        }
+
+        function insertRecord(){
+            
+            try{
+                // Conexion a la base de datos 
+                $pdo = new PDO("mysql:host=$this->server;dbname=$this->dbname;charset=utf8", $this->user, $this->pass);
+                // Consulta sql
+                $sql = "INSERT INTO registro (nombre, apellidos, tiempo, nivel) 
+                            VALUES (?, ?, ?, ?)";
+                
+                // Creamos el prepared statement
+                $smntc = $pdo->prepare($sql);
+                // Añadimos los parametros 
+                $tiempo = floatval($_POST['tiempo']);
+                $dificultad = floatval($_POST['nivel']);
+                $smntc->bindParam(1, $_POST['nombre']);
+                $smntc->bindParam(2, $_POST['apellidos']);
+                $smntc->bindParam(3, $tiempo );
+                $smntc->bindParam(4, $dificultad);
+                // Ejecutamos la consulta
+                $resultc = $smntc->execute();
+        
+            } catch(PDOException $e) {
+               return false;
+            } catch(mysqli_sql_exception $e){
+                return false;
+            }
+            return true;
+        }
+
+        function obtenerMejoresRecords(){
+            try{
+                // Conexion a la base de datos 
+                $pdo = new PDO("mysql:host=$this->server;dbname=$this->dbname;charset=utf8", $this->user, $this->pass);
+                // Consulta sql
+                $sql = "SELECT * FROM registro WHERE nivel = ? ORDER BY tiempo LIMIT 10";
+                
+                // Creamos el prepared statement
+                $smntc = $pdo->prepare($sql);
+                // Añadimos los parametros 
+                $dificultad = floatval($_POST['nivel']);
+                $smntc->bindParam(1, $dificultad);
+                // Ejecutamos la consulta
+                $smntc->execute();
+                // Obtenemos los mejores resultados para devolverlos
+                $resultc = $smntc->fetchAll(PDO::FETCH_ASSOC);
+        
+            } catch(PDOException $e) {
+               return false;
+            } catch(mysqli_sql_exception $e){
+                return false;
+            }
+            return $resultc;
+        }
+    }
+?>
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <!-- Datos que describen el documento -->
+    <meta charset="UTF-8" />
+    <meta name="author" content="Saul Tuñon Fernandez" />
+    <meta name="description" content="Seccion de juego de tiempo de reaccion del F1 Desktop" />
+    <meta name="keywords" content=" f1, formulauno, juego, semaforo" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>F1Desktop - Semaforo</title>
+    
+    <link rel="icon" href="multimedia/imagenes/f1_icono.ico"/>
+    <!-- añadir el elemento link de enlace a la hoja de estilo dentro del <head> del documento html -->
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css" />
+    <link rel="stylesheet" type="text/css" href="estilo/layout.css" />
+    <link rel="stylesheet" type="text/css" href="estilo/semaforo.css" />
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="js/semaforo.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.html">F1 Desktop</a></h1>
+        <nav>
+            <a href="index.html" title="inicio">Inicio</a>
+            <a href="piloto.html" title="piloto">Piloto</a>
+            <a href="noticias.html" title="noticias">Noticias</a>
+            <a href="calendario.html" title="calendario">Calendario</a>
+            <a href="meteorologia.html" title="meteorologia">Meteorologia</a>
+            <a href="circuito.html" title="circuito">Circuito</a>
+            <a href="viajes.html" title="viajes">Viajes</a>
+            <a href="juegos.html" title="juegos" class="active">Juegos</a>
+        </nav>        
+    </header>
+    <p>Estás en: <a href="index.html" title="Enlace a la pagina de inicio de F1Desktop">Inicio</a> >> 
+        <a href="juegos.html" title="Enlace a la pagina de juegos del F1Desktop">Juegos</a> >> 
+        Juego de tiempo de reacción</p>
+    <h2>Juegos</h2>
+    <p>Los juegos que hay disponibles son los siguientes:</p>
+    <ul>
+        <li><a href="memoria.html" title="Enlace a la pagina del juego de memoria">Juego de memoria</a></li>
+        <li><a href="semaforo.php" title="Enlace a la pagina del juego del tiempo de reaccion">Juego de tiempo de reacción</a></li>
+        <li><a href="api.html" title="Enlace a la pagina del parrilla ideal">Parrilla perfecta</a></li>
+    </ul>
+    <main>
+        <script>
+            var semaforo = new Semaforo();
+        </script>
+
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+                $record = new Record();
+                $confirmacion = $record->insertRecord(); 
+                if($confirmacion){
+                    $records = $record->obtenerMejoresRecords(); 
+                    echo '<article>';
+                    echo '<h4>Mejores tiempos</h4>';
+                    echo '<ol>';
+                    foreach($records as $r){
+                        echo '<li>'.$r['nombre'].' '.$r['apellidos'].' - '.$r['tiempo'].'</li>';
+                    }
+                    echo '</ol>';
+                    echo '</article>';
+                }
+            } 
+        ?>
+    </main>
+</body>
+
+</html>
